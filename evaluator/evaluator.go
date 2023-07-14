01d20e65a387e7398b5b9d8502b709f9e8ac065a
Taoning Ge <gtn1024o@outlook.com>
2023-07-14 17:52:53 +0800

    支持布尔比较

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -54,6 +54,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooToBooleanObject(left != right)
 	default:
 		return NULL
 	}
