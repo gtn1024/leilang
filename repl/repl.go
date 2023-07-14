03b2c1edf19eacfb5bf6dbd66f4783406e295ca3
Taoning Ge <gtn1024o@outlook.com>
2023-07-14 17:25:42 +0800

    完善repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"lei/evaluator"
 	"lei/lexer"
 	"lei/parser"
 )
@@ -29,8 +30,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 func printParserErrors(out io.Writer, errors []string) {
