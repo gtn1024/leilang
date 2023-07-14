f1e37f0f4dc4de1a7b6bfe174151a8fe739dfdc6
Taoning Ge <gtn1024o@outlook.com>
2023-07-14 17:31:55 +0800

    支持Boolean

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"lei/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,9 +18,18 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBooToBooleanObject(node.Value)
 	}
 
 	return nil
+}
+
+func nativeBooToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
